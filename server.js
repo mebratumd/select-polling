4daf6a651d45a82c426417628c246de52839ca07
Mathew <umdani49@myumanitoba.ca>
2020-05-25 14:40:24 -0500

    bug

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.set('trust proxy', true); //
 
 
 const https = (req,res,next) => {
-  if (req.headers["x-forwarded-proto"] === "https") {
+  if (req.headers["x-forwarded-proto"] === "https" && req.protocol === "https") {
     return next();
   } else {
     res.redirect(`https://www.selectpolling.ca${req.url}`);
