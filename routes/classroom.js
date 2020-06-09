aeb919492d69d4b08bfeb7d049dc2dc2432d5e28
Mathew <umdani49@myumanitoba.ca>
2020-06-08 21:32:52 -0500

    .

--- routes/classroom.js
+++ routes/classroom.js
@@ -1590,7 +1590,7 @@ router.get("/permission/:id",(req,res,next)=>{
       Election.findOne({_id: req.params.id}).populate({
         path: 'class',
         model: 'Classroom',
-        select: 'students _id name master'
+        select: 'students _id name master partake'
       }).select('-tokens').lean().exec((err,election)=>{
 
         if (err) {
@@ -1619,7 +1619,23 @@ router.get("/permission/:id",(req,res,next)=>{
                 }
               }
               if (idx+1 == election.electionAccess.length) {
-                return res.status(401).send('Unauthorized.');
+                if (req.user.id == election.class.master && (!election.class.partake || election.class.partake == undefined)) {
+
+                  if (election.voteStatus) {
+                    election.voteStatus = election.voteStatus.filter(voterObject => voterObject.email == student.email);
+                  }
+
+                  if (election.electionAccess) {
+                    election.electionAccess = election.electionAccess.filter(accessObject => accessObject.email == student.email);
+                  }
+
+                  let classSize = election.class.students.length;
+
+                  return res.json({election:election,classSize:classSize});
+                } else {
+                  return res.status(401).send('Unauthorized.');
+                }
+
               }
             });
           } else {
